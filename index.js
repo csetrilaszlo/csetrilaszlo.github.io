3c1a08b3c10636664ddd122f9980dbda95d2e3bc
csetrilaszlo <79114152+csetrilaszlo@users.noreply.github.com>
2021-02-18 09:11:10 +0200

    Add files via upload
    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function app(result){
+let seven = document.getElementById('button7').innerHTML = 7;
+let eight = document.getElementById('button8').innerHTML = 8;
+let nine = document.getElementById('button9').innerHTML = 9;
+let four = document.getElementById('button4').innerHTML = 4;
+let five = document.getElementById('button5').innerHTML = 5;
+let six = document.getElementById('button6').innerHTML = 6;
+let one = document.getElementById('button1').innerHTML = 1;
+let two = document.getElementById('button2').innerHTML = 2;
+let three = document.getElementById('button3').innerHTML = 3;
+let zero = document.getElementById('button0').innerHTML = 0;
+let plus = document.getElementById('buttonPlus').innerHTML = '+';
+let minus = document.getElementById('buttonMinus').innerHTML = "-";
+let multiply = document.getElementById('buttonMultiply').innerHTML = 'X';
+let division = document.getElementById('buttonDiv').innerHTML = '/';
+let semi = document.getElementById('buttonSemi').innerHTML = ',';
+
+input.value = input.value + result;
+return result
+}
+
+function ac1(){
+    document.getElementById('input').value= '';
+}
